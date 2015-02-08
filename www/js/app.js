753ddf0811c61ad6ff40390ac4fca2b676a28adf
Luisita <luiyi_1414@hotmail.com>
2015-02-08 17:58:39 -0600

    2

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,12 @@
 // Función para emitir la vibracion
 function vibrar(){
-navigator.notification.alert("El teléfono vibrara",	null,"Vibrar","Empezar");
+	alert("Vibrar Teléfono");
+navigator.notification.alert("Vibrará Teléfono",	null,"Vibrar","Empezar");
 navigator.notification.vibrate(1000);
 }
 // Función para emitir beep
 function beep(){
-navigator.notification.alert('El teléfono emitirá un beep',null,'Beep','Empezar');
+	alert("Sonar beep");
+navigator.notification.alert('Sonara beep',null,'Beep','Empezar');
 navigator.notification.beep(500);
 }
